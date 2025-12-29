daacf54b3ac9ed4e3cbc28c555336ac30a6b7a1b
yashiikaaaa <yashika@truestate.in>
2025-12-29 12:35:06 +0530

    first commit

---
+++ src/sections/Overview.jsx
@@ -0,0 +1,71 @@
+import React from 'react';
+import image from '../assets/gallery/image7.png';
+import Button from '../components/button/buttonMain';
+
+// Overview Component
+export const Overview = ({ contactmodal, setContactModal }) => {
+  return (
+    <div className="bg-PrestigeGrey">
+      <section
+        className="w-full flex flex-wrap items-center justify-center gap-[20px] mx-auto pb-10 md:py-16 px-5 md:px-[7.5rem]"
+        id="Overview"
+      >
+        {/* Overview Text Section */}
+        <div className="flex flex-col justify-center items-center text-center gap-8 h-full md:items-start md:text-left">
+          <h1 className="font-subheading font-normal text-3xl md:text-5xl text-black uppercase">
+            Overview
+          </h1>
+          <p className="max-w-2xl md:text-base text-sm text-black font-body font-light">
+          <span className="font-body font-bold text-xs md:text-lg ">
+        Prestige Marigold
+Phase 2 <p>   </p>
+
+          </span>
+          <br />
+          
+            <span>
+            <p>A thoughtfully
+planned plotted
+development located off
+NH 44, at Bettenahalli near
+Yelahanka, Bengaluru.
+Spread across 716
+residential plots, offered in
+convenient sizes, this new
+phase continues the vision
+of creating a well-organised
+residential community with
+modern infrastructure and
+ample open space</p>
+            <br/>
+            <p>These ready to build plots in Bangalore come with wide internal roads and underground utilities. Whether you’re planning to construct your dream home or invest in residential plots near Bangalore Airport, Prestige Marigold is the perfect place.</p>
+
+            
+
+</span>
+          </p>
+
+          {/* Enquire Now Button using the reusable Button component */}
+          <Button
+                text="Enquire Now!"
+                className=""
+                onClick={() => setContactModal(!contactmodal)} // Toggle contact modal on button click
+              />
+          
+        </div>
+
+        {/* Image and Download Button Section */}
+        <div className="hidden md:flex flex-col items-center">
+          {/* Image Section */}
+          <div className="w-full h-auto flex justify-center border-PrestigeDarkGrey">
+            <img
+              src={image}
+              alt="Prestige Green Brook"
+              className=" w-[420px] h-[300px]"
+            />
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+};
